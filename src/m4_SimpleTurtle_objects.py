664dc9d9e0fca5e868aa5e96b54ce87144405754
scottjm1 <jasminescott2018@gmail.com>
2018-08-30 17:06:52 -0400

    Module 4

--- src/m4_SimpleTurtle_objects.py
+++ src/m4_SimpleTurtle_objects.py
@@ -14,10 +14,10 @@ Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
 """
 ###############################################################################
 #
-# TODO: 1.
+# COMPLETED: 1.
 #   Yes, that means for YOU to DO things per the following instructions:
 #
-#   On Line 13 above, replace  Jasmine_Scott  with your OWN name.
+#   On Line 13 above, replace YOUR_NAME with your OWN name.
 #
 #   BTW, the top block of text above forms what is called a DOC-STRING.
 #   It documents what this module does, in a way that exterior programs
